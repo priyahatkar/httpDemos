2e2939fd167640a5287d6cf92df5ea4fda0a19cd
priyahatkar <124456200+priyahatkar@users.noreply.github.com>
2023-09-16 15:42:23 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './shared/components/dash-board/dash-board.component';
+import { EditAddPostComponent } from './shared/components/edit-add-post/edit-add-post.component';
+import { PostComponent } from './shared/components/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '', component : DashBoardComponent
+  },
+  {
+    path : 'home', component : DashBoardComponent
+  },
+  {
+    path : 'addPost', component : EditAddPostComponent
+  },
+  {
+    path : 'home/:postId', component : PostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { DashBoardComponent } from './shared/components/dash-board/dash-board.component';
+import { EditAddPostComponent } from './shared/components/edit-add-post/edit-add-post.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    PostComponent,
+    EditAddPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
